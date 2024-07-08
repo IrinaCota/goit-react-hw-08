fda034a809a2e24c06d66ca3740d300a21175558
IrinaCota <iryna.kotsa91@gmail.com>
2024-07-08 21:12:26 +0100

    toaster

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,9 +1,11 @@
 import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
+import { Toaster } from 'react-hot-toast';
 
 export default function SignUpPage() {
    return (
     <>
-      <RegistrationForm/>
+       <RegistrationForm />
+       <Toaster />
     </>
   );
 }
